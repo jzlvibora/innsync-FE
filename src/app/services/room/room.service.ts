d499ac50e213abea282bec59c49b8cccdf97addd
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2023-12-10 17:37:19 +0800

    added service for getting avail rooms between checkin-checkout dates

--- src/app/services/room/room.service.ts
+++ src/app/services/room/room.service.ts
@@ -8,13 +8,24 @@ import { Room } from 'src/app/shared/room';
   providedIn: 'root'
 })
 export class RoomService {
-  BASE_URL = environment.apiUrl + 'rooms';
+  BASE_URL = environment.apiUrl;
 
   constructor(private http:HttpClient) { }
   
   public getAllRooms():Observable<Room[]>{
     return this.http.get<Room[]>(
-      this.BASE_URL
+      this.BASE_URL + 'rooms'
+    )
+  }
+
+  public getAvailableRooms(checkInDate:string,checkOutDate:string): Observable<Room[]>{
+    const params = {
+      checkInDate : checkInDate,
+      checkoutDate: checkOutDate
+    }
+    return this.http.get<Room[]>(
+      this.BASE_URL + 'rooms/availableRooms',
+      {params}
     )
   }
 }
