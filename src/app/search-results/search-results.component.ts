61fc4a4f3aa0e8b2865ff633c55d383d27c4ddc9
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2023-12-10 17:35:54 +0800

    added search results component

---
+++ src/app/search-results/search-results.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
+import { Room } from '../shared/room';
+import { RoomService } from '../services/room/room.service';
+
+@Component({
+  selector: 'app-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.scss']
+})
+export class SearchResultsComponent implements OnInit{
+  checkInDate!: string;
+  checkOutDate!:string;
+  rooms!: Room[];
+
+  constructor(private route:ActivatedRoute, private roomService:RoomService ){}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params)=>{
+      this.checkInDate=params['check_in_date'];
+      this.checkOutDate=params['check_in_date'];
+    })
+
+    this.roomService.getAvailableRooms(this.checkInDate,this.checkOutDate).subscribe((res)=>{
+      this.rooms=res;
+    })
+
+
+  }
+  
+
+}
