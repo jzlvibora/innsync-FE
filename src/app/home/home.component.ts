48a748f5c95070b297ebc97fa9aa99bfdce4587b
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2023-12-10 17:35:14 +0800

    updated homepage component and added navigation function to search results page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Room } from '../shared/room';
 import { RoomService } from '../services/room/room.service';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +9,27 @@ import { RoomService } from '../services/room/room.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit{
-  room!:Room[];
-  constructor(private roomService:RoomService){}
+  rooms!:Room[];
+  constructor(private roomService:RoomService, private router:Router){}
 
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
     this.roomService.getAllRooms().subscribe((res)=>{
-      console.log(res)
+      this.rooms=res;
     });
+  }
+
+  navigateToSearchResults(checkInDate:string, checkOutDate:string){
+    this.router.navigate(['/search-results'],
+    {
+      queryParams:{
+        check_in_date:checkInDate,
+        check_out_date:checkOutDate
+      }
+    },
+    
+    )
+
   }
 
 }
