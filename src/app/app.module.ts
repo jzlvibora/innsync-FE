f51fa8c6dded46f538f032159f8ee9b7c556537d
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2023-12-11 16:05:25 +0800

    add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
 import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -30,7 +32,8 @@ import {MatTableModule} from '@angular/material/table';
     HomeComponent,
     HeaderComponent,
     SearchResultsComponent,
-    BookingConfirmationComponent
+    BookingConfirmationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -53,6 +56,7 @@ import {MatTableModule} from '@angular/material/table';
     MatProgressSpinnerModule,
     MatOptionModule,
     MatTableModule,
+    MatIconModule,
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule
