4c6904dbda08da011215c5add99fa869752af77f
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2023-12-11 21:06:48 +0800

    service for creating reservation

--- src/app/services/reservation/reservation.service.ts
+++ src/app/services/reservation/reservation.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Room } from 'src/app/shared/room';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
+  BASE_URL = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  data = {
+    "roomId":2,
+    "checkInDate":"2026-01-01",
+    "checkOutDate":"2026-01-03",
+    "username":"testuser2"
+}
+
+
+  public reserveRoom():Observable<Room>{
+    return this.http.post<any>(
+      this.BASE_URL + 'reservations/' + 'new',
+      this.data
+    )
+  }
 }
